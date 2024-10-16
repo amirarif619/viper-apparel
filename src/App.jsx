a47b311baf1b078e07ecc2db9da0a3bbe43aece6
Amir Arif <amirarif619@gmail.com>
2024-10-16 14:18:44 +0800

    added promotionbanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import HeroBanner from "./components/HeroBanner";
 import MainNavbar from "./components/MainNavbar"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PromotionBanner from "./components/PromotionBanner";
 
 
 function App() {
   return (
     <>
      <MainNavbar />
+     <PromotionBanner/>
     <HeroBanner />
+
     </>
   )
 }
---
+++ src/components/PromotionBanner.jsx
@@ -0,0 +1,53 @@
+import Carousel from 'react-bootstrap/Carousel';
+import './PromotionBanner.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+function PromotionBanner() {
+    const [isPaused, setIsPaused] = useState(false);
+    const [intervalTime, setIntervalTime] = useState(5000);
+
+    const handlePausePlay = () => {
+        if (isPaused) {
+            setIntervalTime(5000); 
+        } else {
+            setIntervalTime(null); 
+        }
+        setIsPaused(!isPaused);
+    };
+        return (
+            <div className="promotion-banner-container">
+          <Carousel 
+          
+          controls={false} 
+          indicators={false} 
+          interval={intervalTime} 
+          className="horizontal-carousel"
+           >
+            <Carousel.Item>
+              <div className="d-flex justify-content-center align-items-center">
+                <p className="m-0"> 🎉 10% STUDENT DISCOUNT 🎉 USE CODE NOSLEEP10 AT CHECKOUT</p>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="d-flex justify-content-center align-items-center">
+                <p className="m-0">🎁 FREE RETURNS ON ALL ORDER 🎁S</p>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="d-flex justify-content-center align-items-center">
+                <p className="m-0">✈️ FREE SHIPPING ON ORDERS OVER $50 ✈️</p>
+              </div>
+            </Carousel.Item>
+          </Carousel>
+          <button className="pause-play-btn" onClick={handlePausePlay}>
+                <FontAwesomeIcon icon={isPaused ? faPlay : faPause} size="sm"/>
+            </button>
+        </div>
+        );
+      }
+      
+      export default PromotionBanner;
+      
